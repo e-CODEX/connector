3a8f13dc0ced45cf1fb2c6eac2e43ba541df96c8
Komi W. KOUDO <komi.koudo.qex@eulisa.europa.eu>
2024-08-13 00:20:12 +0200

    style(starter): improve the code style

--- domibusConnectorStarter/src/main/java/eu/domibus/connector/firststartup/package-info.java
+++ domibusConnectorStarter/src/main/java/eu/domibus/connector/firststartup/package-info.java
@@ -1,9 +1,15 @@
+/*
+ * Copyright 2024 European Union. All rights reserved.
+ * European Union EUPL version 1.1.
+ */
+
 /**
- * This package provides all the business logic that needs to be executed
- * when the connector is started for the first time.
+ * This package provides all the business logic that needs to be executed when the connector is
+ * started for the first time.
  *
- * @since 4.4.12
  * @author komi
  * @version 1.0
+ * @since 4.4.12
  */
+
 package eu.domibus.connector.firststartup;

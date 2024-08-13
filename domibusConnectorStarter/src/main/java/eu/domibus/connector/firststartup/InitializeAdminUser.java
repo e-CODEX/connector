b775e280306d51f2983c911dfa376c6468cf786e
Komi Wolanyo K0UDO <164168359+komi-eulisa@users.noreply.github.com>
2024-08-13 15:03:16 +0200

    style(starter): improve the code style (#79)

--- domibusConnectorStarter/src/main/java/eu/domibus/connector/firststartup/InitializeAdminUser.java
+++ domibusConnectorStarter/src/main/java/eu/domibus/connector/firststartup/InitializeAdminUser.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2024 European Union. All rights reserved.
+ * European Union EUPL version 1.1.
+ */
+
 package eu.domibus.connector.firststartup;
 
 import eu.domibus.connector.persistence.dao.DomibusConnectorUserDao;
@@ -6,60 +11,62 @@ import eu.domibus.connector.persistence.model.PDomibusConnectorUser;
 import eu.domibus.connector.persistence.model.PDomibusConnectorUserPassword;
 import eu.domibus.connector.persistence.model.enums.UserRole;
 import eu.domibus.connector.tools.logging.LoggingMarker;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.transaction.Transactional;
+import javax.xml.bind.DatatypeConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.transaction.Transactional;
-import javax.xml.bind.DatatypeConverter;
-import java.math.BigInteger;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Date;
-
 /**
- * This bean will be run on first startup (for details see FirstStartupCondition)
+ * This bean will be run on first startup (for details see FirstStartupCondition).
  *
- *  It requires a already initialized database
- *  It creates an initial admin user with a random password. Password
- *  will be logged to console
- *
- *
- *
+ * <p>It requires an already initialized database It creates an initial admin user with a random
+ * password. Password will be logged to console
  */
 @Configuration
-@ConditionalOnProperty(prefix = InitializeAdminUserProperties.PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
+@ConditionalOnProperty(
+    prefix = InitializeAdminUserProperties.PREFIX, name = "enabled", havingValue = "true",
+    matchIfMissing = true
+)
 @EnableConfigurationProperties(InitializeAdminUserProperties.class)
 public class InitializeAdminUser {
-
     private static final Logger LOGGER = LogManager.getLogger(InitializeAdminUser.class);
-
     private final InitializeAdminUserProperties initializeAdminUserProperties;
-
-    //instead of the DAOs the service (WebUserPersistenceService) should be used here.
-    //But this requires some refactoring, because the service is deeply integrated in
-    //the webLib Module
+    // instead of the DAOs the service (WebUserPersistenceService) should be used here.
+    // But this requires some refactoring, because the service is deeply integrated in
+    // the webLib Module
     private final DomibusConnectorUserDao userDao;
     private final DomibusConnectorUserPasswordDao userPasswordDao;
 
-
-    public InitializeAdminUser(InitializeAdminUserProperties initializeAdminUserProperties,
-                               DomibusConnectorUserDao userDao,
-                               DomibusConnectorUserPasswordDao userPasswordDao) {
+    /**
+     * Initializes the admin user.
+     *
+     * @param initializeAdminUserProperties The properties required to initialize the admin user.
+     * @param userDao                       The DAO object for interacting with the database for the
+     *                                      DomibusConnectorUser entity.
+     * @param userPasswordDao               The DAO object for interacting with the database for the
+     *                                      DomibusConnectorUserPassword entity.
+     */
+    public InitializeAdminUser(
+        InitializeAdminUserProperties initializeAdminUserProperties,
+        DomibusConnectorUserDao userDao,
+        DomibusConnectorUserPasswordDao userPasswordDao) {
         this.initializeAdminUserProperties = initializeAdminUserProperties;
         this.userDao = userDao;
         this.userPasswordDao = userPasswordDao;
     }
 
     /**
-     * check if the admin user exists in DB
-     *  if not - create it
+     * Check if the admin user exists in DB if not - create it.
      */
     @PostConstruct
     @Transactional
@@ -68,12 +75,15 @@ public class InitializeAdminUser {
 
         PDomibusConnectorUser adminUser = userDao.findOneByUsernameIgnoreCase(adminUserName);
         if (adminUser != null) {
-            LOGGER.info(LoggingMarker.Log4jMarker.CONFIG, "Admin user [{}] already exists in DB", adminUser.getUsername());
+            LOGGER.info(
+                LoggingMarker.Log4jMarker.CONFIG, "Admin user [{}] already exists in DB",
+                adminUser.getUsername()
+            );
             return;
         }
         String newUserPassword = initializeAdminUserProperties.getInitialUserPassword();
 
-        PDomibusConnectorUser newAdminUser = new PDomibusConnectorUser();
+        var newAdminUser = new PDomibusConnectorUser();
         newAdminUser.setUsername(adminUserName);
         newAdminUser.setLocked(false);
         newAdminUser.setRole(UserRole.ADMIN);
@@ -82,7 +92,7 @@ public class InitializeAdminUser {
 
         String dbPassword = generatePasswordHashWithSaltOnlyPW(newUserPassword, salt);
 
-        PDomibusConnectorUserPassword userPassword = new PDomibusConnectorUserPassword();
+        var userPassword = new PDomibusConnectorUserPassword();
         userPassword.setInitialPassword(initializeAdminUserProperties.isInitialChangeRequired());
         userPassword.setCurrentPassword(true);
         userPassword.setPassword(dbPassword);
@@ -92,48 +102,51 @@ public class InitializeAdminUser {
         newAdminUser.getPasswords().add(userPassword);
 
         if (initializeAdminUserProperties.isLogInitialToConsole()) {
-            LOGGER.info(LoggingMarker.Log4jMarker.CONFIG,
-                    "\n###############################" +
-                            "\nSuccessfully created initial admin user [{}] with pw [{}]" +
-                            "\n###############################\n", adminUserName, newUserPassword);
+            LOGGER.info(
+                LoggingMarker.Log4jMarker.CONFIG,
+                """
+                    ###############################
+                    Successfully created initial admin user [{}] with pw [{}]"
+                    ###############################
+                    """,
+                adminUserName,
+                newUserPassword
+            );
         }
 
         userDao.save(newAdminUser);
         userPasswordDao.save(userPassword);
-
     }
 
-
-    //should be done by user service!
+    // should be done by user service!
     private static String generatePasswordHashWithSaltOnlyPW(String password, String saltParam)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        int iterations = 1000;
-        char[] chars = password.toCharArray();
-        byte[] salt = DatatypeConverter.parseHexBinary(saltParam);
-        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] hash = skf.generateSecret(spec).getEncoded();
+        throws NoSuchAlgorithmException, InvalidKeySpecException {
+        var iterations = 1000;
+        var chars = password.toCharArray();
+        var salt = DatatypeConverter.parseHexBinary(saltParam);
+        var spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        var secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        var hash = secretKeyFactory.generateSecret(spec).getEncoded();
         return toHex(hash);
     }
 
-    //should be done by user service!
+    // should be done by user service!
     private static String getHexSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
+        var secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        var salt = new byte[16];
+        secureRandom.nextBytes(salt);
         return toHex(salt);
     }
 
-    //should be done by user service!
-    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
+    // should be done by user service!
+    private static String toHex(byte[] array) {
+        var bigInteger = new BigInteger(1, array);
+        var hex = bigInteger.toString(16);
+        var paddingLength = (array.length * 2) - hex.length();
         if (paddingLength > 0) {
-            return String.format("%0" + paddingLength + "d", 0) + hex;
+            return String.format("%0" + paddingLength + "d%s", 0) + hex;
         } else {
             return hex;
         }
     }
-
 }

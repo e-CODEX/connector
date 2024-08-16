66911da6a8ceb6d4733329c9352b68145e871df9
Komi Wolanyo K0UDO <164168359+komi-eulisa@users.noreply.github.com>
2024-08-16 11:52:13 +0200

    docs(all): improve the copyright (#84)

--- domibusConnectorPersistence/src/main/java/eu/domibus/connector/persistence/dao/DomibusConnectorServiceDao.java
+++ domibusConnectorPersistence/src/main/java/eu/domibus/connector/persistence/dao/DomibusConnectorServiceDao.java
@@ -1,6 +1,11 @@
 /*
- * Copyright 2024 European Union. All rights reserved.
- * European Union EUPL version 1.1.
+ * Copyright 2024 European Union Agency for the Operational Management of Large-Scale IT Systems
+ * in the Area of Freedom, Security and Justice (eu-LISA)
+ *
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy at: https://joinup.ec.europa.eu/software/page/eupl
  */
 
 package eu.domibus.connector.persistence.dao;

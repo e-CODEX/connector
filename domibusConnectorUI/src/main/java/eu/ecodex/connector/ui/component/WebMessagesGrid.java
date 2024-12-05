151c16a7ccf6ea9f2301bc1ec03996d2bb441d90
Komi Wolanyo K0UDO <164168359+komi-eulisa@users.noreply.github.com>
2024-12-05 13:59:48 +0100

    fix(ui): grid data providers (#128)

--- domibusConnectorUI/src/main/java/eu/ecodex/connector/ui/component/WebMessagesGrid.java
+++ domibusConnectorUI/src/main/java/eu/ecodex/connector/ui/component/WebMessagesGrid.java
@@ -260,7 +260,7 @@ public class WebMessagesGrid extends PagingGrid<WebMessage> implements AfterNavi
         var sort = Sort.by(collect.toArray(new Sort.Order[] {}));
 
         // creating page request with sort order and offset
-        var pageRequest = PageRequest.of(offset, limit, sort);
+        var pageRequest = PageRequest.of(offset / limit, limit, sort);
         LOGGER.debug("PageRequest: {}", pageRequest);
         var all = dcMessagePersistenceService.findAll(createExample(), pageRequest);
         LOGGER.debug("Page requested size: {}", all.getSize());
